da3ce4c336c02b526c116ae3bffe1b94547f89fb
Alina Mokrova <e1325014@student.tuwien.ac.at>
2023-05-04 19:35:51 +0200

    testing out if THREEJS works online

--- index.js
+++ index.js
@@ -1,24 +1,103 @@
-const canvas = document.querySelector('#canvas');
-const renderer = new THREE.WebGLRenderer({ canvas });
+// Three.js - Load .OBJ and .MTL file
+// from https://threejsfundamentals.org/threejs/threejs-load-obj-materials.html
 
-const scene = new THREE.Scene();
 
-const objLoader = new THREE.OBJLoader();
-const mtlLoader = new THREE.MTLLoader();
+'use strict';
 
-// load the material file
-mtlLoader.load('movingGeorge.mtl', function (materials) {
-    materials.preload();
-    // load the object file, passing in the materials
-    objLoader.setMaterials(materials);
-    objLoader.load('movingGeorge.obj', function (object) {
-        scene.add(object);
-    });
-});
+/* global THREE */
 
-function animate() {
-    requestAnimationFrame(animate);
-    // apply transformations to the object here
-    renderer.render(scene, camera);
+function main() {
+    const canvas = document.querySelector('#c');
+    const renderer = new THREE.WebGLRenderer({ canvas });
+
+    const fov = 45;
+    const aspect = 2;  // the canvas default
+    const near = 0.1;
+    const far = 100;
+    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.set(0, 10, 20);
+
+    const controls = new THREE.OrbitControls(camera, canvas);
+    controls.target.set(0, 5, 0);
+    controls.update();
+
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color('black');
+
+    {
+        const planeSize = 40;
+
+        const loader = new THREE.TextureLoader();
+        const texture = loader.load('https://threejsfundamentals.org/threejs/resources/images/checker.png');
+        texture.wrapS = THREE.RepeatWrapping;
+        texture.wrapT = THREE.RepeatWrapping;
+        texture.magFilter = THREE.NearestFilter;
+        const repeats = planeSize / 2;
+        texture.repeat.set(repeats, repeats);
+
+        const planeGeo = new THREE.PlaneBufferGeometry(planeSize, planeSize);
+        const planeMat = new THREE.MeshPhongMaterial({
+            map: texture,
+            side: THREE.DoubleSide,
+        });
+        const mesh = new THREE.Mesh(planeGeo, planeMat);
+        mesh.rotation.x = Math.PI * -.5;
+        scene.add(mesh);
+    }
+
+    {
+        const skyColor = 0xB1E1FF;  // light blue
+        const groundColor = 0xB97A20;  // brownish orange
+        const intensity = 1;
+        const light = new THREE.HemisphereLight(skyColor, groundColor, intensity);
+        scene.add(light);
+    }
+
+    {
+        const color = 0xFFFFFF;
+        const intensity = 1;
+        const light = new THREE.DirectionalLight(color, intensity);
+        light.position.set(5, 10, 2);
+        scene.add(light);
+        scene.add(light.target);
+    }
+
+    {
+        const objLoader = new THREE.OBJLoader2();
+        objLoader.loadMtl('https://threejsfundamentals.org/threejs/resources/models/windmill/windmill.mtl', null, (materials) => {
+            objLoader.setMaterials(materials);
+            objLoader.load('https://threejsfundamentals.org/threejs/resources/models/windmill/windmill.obj', (event) => {
+                const root = event.detail.loaderRootNode;
+                scene.add(root);
+            });
+        });
+    }
+
+    function resizeRendererToDisplaySize(renderer) {
+        const canvas = renderer.domElement;
+        const width = canvas.clientWidth;
+        const height = canvas.clientHeight;
+        const needResize = canvas.width !== width || canvas.height !== height;
+        if (needResize) {
+            renderer.setSize(width, height, false);
+        }
+        return needResize;
+    }
+
+    function render() {
+
+        if (resizeRendererToDisplaySize(renderer)) {
+            const canvas = renderer.domElement;
+            camera.aspect = canvas.clientWidth / canvas.clientHeight;
+            camera.updateProjectionMatrix();
+        }
+
+        renderer.render(scene, camera);
+
+        requestAnimationFrame(render);
+    }
+
+    requestAnimationFrame(render);
 }
-animate();
+
+main();
